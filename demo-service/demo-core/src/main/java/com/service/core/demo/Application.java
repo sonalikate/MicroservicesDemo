ba7ceb413eda41ab13898324568ea1eaf860295b
sonalikate <sonalikate@gmail.com>
2020-09-03 17:43:18 +0530

    Create Application.java

---
+++ demo-service/demo-core/src/main/java/com/service/core/demo/Application.java
@@ -0,0 +1,16 @@
+package com.service.core.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starting Point of Service.
+ * 
+ * @author Sonali Kate
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
