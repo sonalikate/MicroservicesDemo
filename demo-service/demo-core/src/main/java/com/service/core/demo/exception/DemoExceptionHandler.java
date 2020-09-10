070550079ec8d3735a7cf1d2796b2da80366e9b0
sonalikate <sonalikate@gmail.com>
2020-09-10 10:00:14 +0530

    Update DemoExceptionHandler.java

--- demo-service/demo-core/src/main/java/com/service/core/demo/exception/DemoExceptionHandler.java
+++ demo-service/demo-core/src/main/java/com/service/core/demo/exception/DemoExceptionHandler.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.demo.common.exception.GenericException;
+
 /**
  * Exception handler class to define exceptions.
  * 
