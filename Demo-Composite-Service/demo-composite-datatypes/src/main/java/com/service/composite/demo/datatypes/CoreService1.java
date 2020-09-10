4b882a145701c59d79abebcbe6f50b3e7af8f6d2
sonalikate <sonalikate@gmail.com>
2020-09-10 10:54:01 +0530

    Create CoreService1.java

---
+++ Demo-Composite-Service/demo-composite-datatypes/src/main/java/com/service/composite/demo/datatypes/CoreService1.java
@@ -0,0 +1,7 @@
+package com.service.composite.demo.datatypes;
+
+//TODO - Rename the class name and replace the variable part <coreservice> with the actual service name. Remove this comment.
+
+public class CoreService1 {
+
+}
