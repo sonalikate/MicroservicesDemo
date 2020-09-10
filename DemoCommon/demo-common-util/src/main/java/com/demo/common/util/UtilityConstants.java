2dbb18be9e665f972910fe9e4330c5ea4fd85cf0
sonalikate <sonalikate@gmail.com>
2020-09-10 10:35:48 +0530

    Create UtilityConstants.java

---
+++ DemoCommon/demo-common-util/src/main/java/com/demo/common/util/UtilityConstants.java
@@ -0,0 +1,21 @@
+package com.demo.common.util;
+
+/**
+ * 
+ * @author Sonali Kate
+ *
+ */
+public class UtilityConstants {
+
+	private UtilityConstants() {
+
+	}
+
+	public static final String LOG_CO_RELATION_ID = "logCoRelationId";
+
+	public static final java.lang.String BEAN_ID_LOAD_BALANCED_REST_TEMPLATE = "load-balanced-template";
+
+	public static final java.lang.String BEAN_ID_DEFAULT_REST_TEMPLATE = "default-rest-template";
+
+	public static final java.lang.String BEAN_ID_DEFAULT_ASYNC_EXECUTOR = "default-async-executor";
+}
