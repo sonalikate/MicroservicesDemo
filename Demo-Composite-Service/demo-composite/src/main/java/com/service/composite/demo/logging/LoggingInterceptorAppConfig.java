7fad3791380c74b3201b8542c3d126b46bafcf79
sonalikate <sonalikate@gmail.com>
2020-09-10 10:43:58 +0530

    Create LoggingInterceptorAppConfig.java

---
+++ Demo-Composite-Service/demo-composite/src/main/java/com/service/composite/demo/logging/LoggingInterceptorAppConfig.java
@@ -0,0 +1,23 @@
+package com.tsystem.nemo.service.composite.compositeservice.api.logging;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Register interceptor with the interceptor registry
+ * @author Sonali Kate
+ *
+ */
+@Component
+public class LoggingInterceptorAppConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private LoggingInterceptor loggingInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(loggingInterceptor);
+	}
+}
