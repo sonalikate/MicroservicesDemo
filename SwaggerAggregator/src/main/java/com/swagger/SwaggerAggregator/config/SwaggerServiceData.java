157dd266fa84a2edac2f1a388cc6a1d77f379790
sonalikate <sonalikate@gmail.com>
2020-09-10 14:44:28 +0530

    Create SwaggerServiceData.java

---
+++ SwaggerAggregator/src/main/java/com/swagger/SwaggerAggregator/config/SwaggerServiceData.java
@@ -0,0 +1,31 @@
+package com.swagger.SwaggerAggregator.config;
+
+public class SwaggerServiceData {
+	String name;
+	String url;
+	String version;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+}
