9a264b29bb197c6e15e4dc219b68bed96db7baf9
sonalikate <sonalikate@gmail.com>
2020-09-10 14:47:55 +0530

    Create IConstants.java

---
+++ SwaggerAggregator/src/main/java/com/swagger/SwaggerAggregator/utils/IConstants.java
@@ -0,0 +1,17 @@
+package com.swagger.SwaggerAggregator.utils;
+
+public interface IConstants {
+	String PDF_FILE_EXT = "pdf";
+
+	String ANSIDOC_FILE_EXT = ".adoc";
+
+	String HTML5_FILE_EXT = "html5";
+
+	String HTML_FILE_EXT = "html";
+
+	String coverPageImg = "cover.jpg";
+
+	String SLASH = "/";
+
+	String DOT = ".";
+}
