bdbf36cb4c057c06910dabd273b875009991e0c1
sonalikate <sonalikate@gmail.com>
2020-09-10 10:40:06 +0530

    Create Application.java

---
+++ Demo-Composite-Service/demo-composite/src/main/java/com/service/composite/demo/Application.java
@@ -0,0 +1,25 @@
+package com.service.composite.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//TODO - Rename the class name and replace the variable part <compositeservice> with the actual service name. Remove this comment.
+
+/**
+* Application entry class to launch the service.
+* 
+* @author Sonali Kate
+*
+*/
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * Run the application.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
