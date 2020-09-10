7aad91e33866dfe6d1f950125ef71fd6cabedd3c
sonalikate <sonalikate@gmail.com>
2020-09-10 10:42:00 +0530

    Create ApiConstants.java

---
+++ Demo-Composite-Service/demo-composite/src/main/java/com/service/composite/demo/utils/ApiConstants.java
@@ -0,0 +1,15 @@
+package com.service.composite.demo.utils;
+
+public class ApiConstants {
+
+	private ApiConstants() {
+
+	}
+
+	public static final String CORE_SERVICE_1_BASE_URI = "http://core1-service/core1-service/1.0";
+	public static final String CORE_SERVICE_2_BASE_URI = "http://core2-service/core2-service/1.0";
+	public static final String CORE_SERVICE1_API = "/generator/create/{idType}";
+	public static final String CORE_SERVICE2_API = "/create";
+	public static final String CORE_SERVICE_1 = "core1-service";
+	public static final String CORE_SERVICE_2 = "core2-service";
+}
